f65865ff72178c8bde1b0dc4b5e7dd73e18e8fe7
Sergio Mendoza <smendoza787@gmail.com>
2017-09-18 22:49:12 -0700

    setup mongoose connection in server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Pet = require('./api/models/petModel'),
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Petdb', { useMongoClient: true });
+
+app.use(bodyParser.urlencoded({ extended: true })); //applying bodyParser middleware
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/petRoutes'); //importing route
+routes(app); //register the route
 
 app.listen(port);
 
